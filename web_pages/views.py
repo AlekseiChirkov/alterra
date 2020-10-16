53fb4bfb8a67885335f5d799495156899fc36685
Alex <tektonikboy98@gmail.com>
2020-10-16 18:25:19 +0600

    Ok

--- web_pages/views.py
+++ web_pages/views.py
@@ -4,6 +4,6 @@ from web_pages.models import Recommendation
 
 
 def index(request):
-    recommendations = Recommendation.objects.order_by('date')[:7]
+    recommendations = Recommendation.objects.order_by('date')[:9]
     context = {'recommendations': recommendations}
     return render(request, 'web_pages/index.html', context)
